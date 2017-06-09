b2292c4f141eab8889a4e51a88955f2f806f5a15
Lang Li <lli76@be-linux02.lt.ucsc.edu>
2017-06-09 10:25:08 -0700

    Multithreading complete

--- Multithreading/increment.c
+++ Multithreading/increment.c
@@ -1,47 +1,96 @@
-# include <pthreads.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <getopt.h>
 
-// Below skeleton code is generously provided by Darrell Long
-int someSharedVariable;
+// Below skeleton code is generously provided by Yash or Darrell or whoever created the pdf
+int incrementor;
 pthread_mutex_t mutex;
 
 struct threadArgs
 {
-  int data;
+	int data;
+}threadArgs;
+
+void safeIncrement()
+{
+ 	 pthread_mutex_lock(&mutex);
+
+  	// Perform stuff on someSharedVariable
+  	incrementor += 1;
+
+  	pthread_mutex_unlock(&mutex);
+
+	return;
 }
 
 void *threadFunc(void *myArgs)
 {
-  struct threadArgs *args = (struct threadArgs *) myArgs;
-  int data = args->data;
-
-  someWork();
+	struct threadArgs *args = (struct threadArgs *) myArgs;
+  	int numberOfCalls = args->data;
+	// Jurik helped me on this part
+	for(int i = 0; i < numberOfCalls; i++)
+	{
+  		safeIncrement();
+	}
+	return NULL;
 }
 
-void someWork()
+
+int main(int argc, char **argv)
 {
-  pthread_mutex_lock(&mutex);
+	// use getopt to get how many threads should be spawned
+  	int numThreads;
+	pthread_t *thisThread;
+	int input;
+	while((input = getopt(argc, argv, "n:")) != -1)
+	{
+		switch(input)
+		{
+			case 'n':
+			{
+				// get the number that runs runs the threading
+				numThreads = atoi(optarg);
+				break;
+			}
+			default:
+			{
+				printf("Invalid flag");
+				break;
+			}
+		}
+	}
+	
+	// Ishani and Boris helped me on explaining this concept
+	thisThread = calloc(numThreads, sizeof(pthread_t));
+	struct threadArgs *args = calloc(numThreads, sizeof(threadArgs));
 
-  // Perform stuff on someSharedVariable
+  	// Init mutex
+	// Ishani and Jurik helped me on this part of code
+  	pthread_mutex_init(&mutex, NULL);
 
-  pthread_mutex_unlock(&mutex);
-}
-int main(void)
-{
-  pthread_t thisThread;
-  struct threadArgs args;
+  	// Set Arguments and create thread
+	for(int i = 0; i < numThreads; i++)
+	{
+		// Jurik and Ishani helped me to complete this code
+		args[i].data = 1000;
+  		pthread_create(&thisThread[i], NULL, threadFunc, &args[i]);
+	}
 
-  // Init mutex
-  pthread_mutex_init(&mutex);
+  	// Wait for thread to finish executing
+	for(int i = 0; i < numThreads; i++)
+	{
+  		pthread_join(thisThread[i], NULL);
+	}
 
-  // Set Arguments and create thread
-  args.data = 10;
-  pthread_create(&thisThread, NULL, threadFunc, &args);
+  	// Destroy mutex
+  	pthread_mutex_destroy(&mutex);
 
-  // Wait for thread to finish executing
-  pthread_join(thisThread, NULL);
+	// Free allocated memory
+	free(thisThread);
+	free(args);
 
-  // Destroy mutex
-  pthread_mutex_destroy(&mutex);
-
-  return 0;
+	// Print out the final result of the thread addition
+	printf("%d\n", incrementor);
+  	return 0;
 }
